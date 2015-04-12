0fddae97e8904a71b5042a3c25358e4428069635
aritaku19@gmail.com <aritaku19@gmail.com>
2015-04-12 12:04:41 +0900

    first commit

---
+++ rails_api_with_tableview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rails_api_with_tableview
+//
+//  Created by 有村 琢磨 on 2015/04/12.
+//  Copyright (c) 2015年 有村 琢磨. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
